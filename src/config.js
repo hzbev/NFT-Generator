f6af4b29bb70f42dbb23cb2e74d85896eeede763
Ap0mano <apomanko1@gmail.com>
2023-01-14 19:20:27 +0100

    first commit

--- src/config.js
+++ src/config.js
@@ -6,22 +6,21 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
-const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
+const namePrefix = "Prefix";
+const description = "desc";
+const baseUri = "ipfs://"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 30,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Body" },
+      { name: "Outline" },
+      { name: "Eyes" },
+      { name: "Mouth" },
+      { name: "Acceessories" }
     ],
   },
 ];
@@ -31,13 +30,13 @@ const shuffleLayerConfigurations = false;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 1000,
+  height: 1000,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  // external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -115,7 +114,7 @@ const pixelFormat = {
 };
 
 const background = {
-  generate: true,
+  generate: false,
   brightness: "80%",
   static: false,
   default: "#000000",
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const basePath = process.cwd();
 const { NETWORK } = require(`${basePath}/constants/network.js`);
 const fs = require("fs");
+const Jimp = require('jimp');
+const sharp = require('sharp');
+const path = require("path");
 const sha1 = require(`${basePath}/node_modules/sha1`);
 const { createCanvas, loadImage } = require(`${basePath}/node_modules/canvas`);
 const buildDir = `${basePath}/build`;
@@ -24,6 +27,10 @@ const {
 } = require(`${basePath}/src/config.js`);
 const canvas = createCanvas(format.width, format.height);
 const ctx = canvas.getContext("2d");
+
+const canvas1 = createCanvas(format.width, format.height);
+const ctx1 = canvas1.getContext("2d");
+
 ctx.imageSmoothingEnabled = format.smoothing;
 var metadataList = [];
 var attributesList = [];
@@ -130,59 +137,29 @@ const addMetadata = (_dna, _edition) => {
   let tempMetadata = {
     name: `${namePrefix} #${_edition}`,
     description: description,
-    file_url: `${baseUri}/${_edition}.png`,
     image: `${baseUri}/${_edition}.png`,
     attributes: attributesList,
     custom_fields: {
-      dna: sha1(_dna),
       edition: _edition,
-      date: dateTime,
-      compiler: "HashLips Art Engine - codeSTACKr Modified",
     },
     ...extraMetadata,
   };
-  if (network == NETWORK.sol) {
-    tempMetadata = {
-      //Added metadata for solana
-      name: tempMetadata.name,
-      symbol: solanaMetadata.symbol,
-      description: tempMetadata.description,
-      //Added metadata for solana
-      seller_fee_basis_points: solanaMetadata.seller_fee_basis_points,
-      image: `image.png`,
-      //Added metadata for solana
-      external_url: solanaMetadata.external_url,
-      edition: _edition,
-      ...extraMetadata,
-      attributes: tempMetadata.attributes,
-      properties: {
-        files: [
-          {
-            uri: "image.png",
-            type: "image/png",
-          },
-        ],
-        category: "image",
-        creators: solanaMetadata.creators,
-      },
-    };
-  }
   metadataList.push(tempMetadata);
   attributesList = [];
 };
 
-const addAttributes = (_element) => {
-  let selectedElement = _element.layer.selectedElement;
+const addAttributes = (_type, _value) => {
   attributesList.push({
-    trait_type: _element.layer.name,
-    value: selectedElement.name,
+    trait_type: _type,
+    value: _value,
   });
 };
 
 const loadLayerImg = async (_layer) => {
   return new Promise(async (resolve) => {
-    const image = await loadImage(`${_layer.selectedElement.path}`);
-    resolve({ layer: _layer, loadedImage: image });
+    loadImage(_layer.selectedElement.path).then((image) => {
+      resolve({ layer: _layer, loadedImage: image });
+    })
   });
 };
 
@@ -197,20 +174,24 @@ const addText = (_sig, x, y, size) => {
 const drawElement = (_renderObject, _index, _layersLen) => {
   ctx.globalAlpha = _renderObject.layer.opacity;
   ctx.globalCompositeOperation = _renderObject.layer.blend;
+
+
   text.only
     ? addText(
-        `${_renderObject.layer.name}${text.spacer}${_renderObject.layer.selectedElement.name}`,
-        text.xGap,
-        text.yGap * (_index + 1),
-        text.size
-      )
+      `${_renderObject.layer.name}${text.spacer}${_renderObject.layer.selectedElement.name}`,
+      text.xGap,
+      text.yGap * (_index + 1),
+      text.size
+    )
     : ctx.drawImage(
-        _renderObject.loadedImage,
-        0,
-        0,
-        format.width,
-        format.height
-      );
+      _renderObject.loadedImage,
+      0,
+      0,
+      format.width,
+      format.height
+    );
+
+
 
   addAttributes(_renderObject);
 };
@@ -289,8 +270,7 @@ const createDna = (_layers) => {
       random -= layer.elements[i].weight;
       if (random < 0) {
         return randNum.push(
-          `${layer.elements[i].id}:${layer.elements[i].filename}${
-            layer.bypassDNA ? "?bypassDNA=true" : ""
+          `${layer.elements[i].id}:${layer.elements[i].filename}${layer.bypassDNA ? "?bypassDNA=true" : ""
           }`
         );
       }
@@ -307,8 +287,8 @@ const saveMetaDataSingleFile = (_editionCount) => {
   let metadata = metadataList.find((meta) => meta.custom_fields.edition == _editionCount);
   debugLogs
     ? console.log(
-        `Writing metadata for ${_editionCount}: ${JSON.stringify(metadata)}`
-      )
+      `Writing metadata for ${_editionCount}: ${JSON.stringify(metadata)}`
+    )
     : null;
   fs.writeFileSync(
     `${buildDir}/json/${_editionCount}.json`,
@@ -364,40 +344,84 @@ const startCreating = async () => {
           loadedElements.push(loadLayerImg(layer));
         });
 
-        await Promise.all(loadedElements).then((renderObjectArray) => {
+        await Promise.all(loadedElements).then(async (renderObjectArray) => {
           debugLogs ? console.log("Clearing canvas") : null;
-          ctx.clearRect(0, 0, format.width, format.height);
-          if (gif.export) {
-            hashlipsGiffer = new HashlipsGiffer(
-              canvas,
-              ctx,
-              `${buildDir}/gifs/${abstractedIndexes[0]}.gif`,
-              gif.repeat,
-              gif.quality,
-              gif.delay
-            );
-            hashlipsGiffer.start();
-          }
-          if (background.generate) {
-            drawBackground();
-          }
-          renderObjectArray.forEach((renderObject, index) => {
-            drawElement(
-              renderObject,
-              index,
-              layerConfigurations[layerConfigIndex].layersOrder.length
-            );
-            if (gif.export) {
-              hashlipsGiffer.add();
+
+          let toComposite = []
+          let hexToUse = "#000000";
+          for await (const renderObject of renderObjectArray) {
+          addAttributes(renderObject.layer.name, renderObject.layer.selectedElement.name);
+            // console.log(renderObject.layer.selectedElement)
+            if (renderObject.layer.name == 'Outline' && renderObject.layer.selectedElement.name == "Black") hexToUse = "#000000"
+            if (renderObject.layer.name == 'Background') continue;
+            if (renderObject.layer.name == 'Body') {
+              switch (renderObject.layer.selectedElement.name) {
+                case "Toxic Green":
+                  hexToUse = "#93c787"
+                  break;
+                case "Baby Blue":
+                  hexToUse = "#96d2d6"
+                  break;
+                case "Iridescent":
+                  hexToUse = "#bab5c7"
+                  break;
+                case "Baby Pink":
+                  hexToUse = "#c4b3c9"
+                  break;
+                case "Peach":
+                  hexToUse = "#e8b2a7"
+                  break;
+                case "Lilac":
+                  hexToUse = "#534f6e"
+                  break;
+                case "Beach":
+                  hexToUse = "#737b9c"
+                  break;
+                case "Purpel":
+                  hexToUse = "#2a2d4d"
+                  break;
+                case "Green":
+                  hexToUse = "#476149"
+                  break;
+                case "Neon Pink":
+                  hexToUse = "#bd4da8"
+                  break;
+                case "Fiery Red":
+                  hexToUse = "#ff4c70"
+                  break;
+                case "White":
+                  hexToUse = "#565d6b"
+                  break;
+                case "Rose":
+                  hexToUse = "#b39a92"
+                  break;
+                case "Blue":
+                  hexToUse = "#5e828a"
+                  break;
+                case "Bubblegum":
+                  hexToUse = "#7a6977"
+                  break;
+                case "Gray":
+                  hexToUse = "#4a4a4a"
+                  break;
+              }
             }
-          });
-          if (gif.export) {
-            hashlipsGiffer.stop();
+            if (renderObject.layer.name == 'Mouth') {
+              toComposite.push({ input: await changeColor(hexToUse, renderObject.layer.selectedElement.path) })
+            } else if (renderObject.layer.name == 'Eyes' || renderObject.layer.name == 'Acceessories') {
+              toComposite.push({ input: await changeColor(hexToUse, renderObject.layer.selectedElement.path) })
+            } else {
+              toComposite.push({ input: renderObject.layer.selectedElement.path })
+            }
           }
+
+          sharp(renderObjectArray[0].layer.selectedElement.path)
+            .composite(toComposite)
+            .toFile(`${buildDir}/images/${abstractedIndexes[0]}.png`);
           debugLogs
             ? console.log("Editions left to create: ", abstractedIndexes)
             : null;
-          saveImage(abstractedIndexes[0]);
+          // saveImage(abstractedIndexes[0]);
           addMetadata(newDna, abstractedIndexes[0]);
           saveMetaDataSingleFile(abstractedIndexes[0]);
           console.log(
@@ -424,5 +448,34 @@ const startCreating = async () => {
   }
   writeMetaData(JSON.stringify(metadataList, null, 2));
 };
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+async function changeColor(hex, imagePath) {
+  const newColor = hexToRgb(hex);
+
+  const image = await Jimp.read(imagePath);
+  const imageData = image.bitmap.data;
+
+  for (let i = 0; i < imageData.length; i += 4) {
+    const r = imageData[i];
+    const g = imageData[i + 1];
+    const b = imageData[i + 2];
+
+    if (r === 0 && g === 0 && b === 0) {
+      imageData[i] = newColor.r //r
+      imageData[i + 1] = newColor.g //g
+      imageData[i + 2] = newColor.b  //b
+    }
+  }
+  return await image.getBufferAsync(Jimp.MIME_PNG);
+}
 
 module.exports = { startCreating, buildSetup, getElements };
